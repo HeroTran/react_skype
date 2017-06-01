37bcd71f145fbbc74365346e708e834d280b1d7e
hung.tran <hung.tran@zinio.com>
2017-06-01 17:37:13 +0700

    Changed something

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,6 @@
 import * as types from '../constants/ActionTypes'
 
-/*export const addTodo = text => ({ type: types.ADD_TODO, text })
-export const deleteTodo = id => ({ type: types.DELETE_TODO, id })
-export const editTodo = (id, text) => ({ type: types.EDIT_TODO, id, text })
-export const completeTodo = id => ({ type: types.COMPLETE_TODO, id })
-export const completeAll = () => ({ type: types.COMPLETE_ALL })
-export const clearCompleted = () => ({ type: types.CLEAR_COMPLETED })
 
-*/
 export const showListIcon = (iconList) => ({type:types.SHOW_ALL_ICON,payload:iconList})
 export const onClickCheckIcon = (idx) => ({type:types.SELECTED_ICON,payload:idx})
 export const onmousedown = (parent_id,idx)=>({type:types.CHECK_ICON_DOWN,parent_id,idx})
